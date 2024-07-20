578933d16e1a383698ba299ed1229af84d85ed90
Bqnic <123201175+Bqnic@users.noreply.github.com>
2024-07-20 17:31:14 +0200

    make a better plotter

--- case/city_positions.py
+++ case/city_positions.py
@@ -1,14 +1,12 @@
-import numpy as np
-
 #change this array for another case
 city_coordinates = [
-    np.array([1, 1]),
-    np.array([2, 3]),
-    np.array([3, 2]),
-    np.array([4, 1]),
-    np.array([6, 1]),
-    np.array([6, 3]),
-    np.array([4, 5])
+    [1, 1], #x coordinate, y coordinate
+    [2, 3],
+    [3, 2],
+    [4, 1],
+    [6, 1],
+    [6, 3],
+    [4, 5]
 ]
 
 def get_cities():
--- case/plotter.py
+++ case/plotter.py
@@ -5,4 +5,10 @@ cities = city_positions.get_cities()
 
 for city in cities:
     plt.plot(city[0], city[1], 'o')
+
+for i, (x1, y1) in enumerate(cities):
+    for j, (x2, y2) in enumerate(cities):
+        if i < j:  
+            plt.plot([x1, x2], [y1, y2], color='gray', linestyle='-', linewidth=0.5)
+
 plt.show()
