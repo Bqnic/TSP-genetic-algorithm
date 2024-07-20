42d712f415296b2443745ba5165ac64112bf9b0b
Bqnic <123201175+Bqnic@users.noreply.github.com>
2024-07-20 22:57:00 +0200

    add barebones of roulette selection, sort the population according to their fitness value

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -9,28 +9,66 @@ cities = get_cities()
 
 INITIAL_POPULATION_SIZE = 7
 
-initial_population = []
+population = []
 
 for i in range(0, INITIAL_POPULATION_SIZE):
     random_traversal_order = cities.copy()
     random.shuffle(random_traversal_order)
     chromosome = Chromosome(random_traversal_order)
 
-    initial_population.append(chromosome)
+    population.append(chromosome)
 
-#readjusting fitness values
-min_fitness_value = initial_population[0].fitness_value
-for chromosome in initial_population:
+#readjusting fitness values to be > 0
+min_fitness_value = population[0].fitness_value
+for chromosome in population:
     if chromosome.fitness_value < min_fitness_value:
         min_fitness_value = chromosome.fitness_value
 
 min_fitness_value = min_fitness_value - 1
-for chromosome in initial_population:
+for chromosome in population:
     chromosome.fitness_value -= min_fitness_value 
 
+#sorting chromosomes in population according to their fitness value
+population.sort(key=lambda chromosome: chromosome.fitness_value, reverse=True)
+
+#roulette selection
+total_fitness = 0
+for chromosome in population:
+    total_fitness += chromosome.fitness_value
+
+cum_sum = []
+for i in range(0, len(population) - 1):
+    sum = 0
+    for j in range (i, len(population)):
+        sum += population[j].fitness_value / total_fitness
+    cum_sum.append(round(sum, 4))
+cum_sum.append(0)
+
+parent1 = 'x'
+parent2 = 'x'
+roulette_ball = random.uniform(0, 1)
+for i in range(1, len(cum_sum)):
+    if roulette_ball >= cum_sum[i]:
+        parent1 = i - 1
+        print(roulette_ball)
+        print(parent1)
+        break
+roulette_ball = random.uniform(0, 1)
+for i in range(1, len(cum_sum)):
+    if roulette_ball >= cum_sum[i]:
+        parent2 = i - 1
+        print(roulette_ball)
+        print(parent2)
+        break
+
 print("INITIAL POPULATION")
-for chromosome in initial_population:
+for chromosome in population:
     print(chromosome.__str__())
+
+print(cum_sum)
+
+
+
 
 
 
