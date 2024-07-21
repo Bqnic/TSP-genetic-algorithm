0965235867ba1206633fecf5921cc6360f01fdf4
Bqnic <123201175+Bqnic@users.noreply.github.com>
2024-07-21 10:04:11 +0200

    add picking parents for next generation

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,5 +1,6 @@
 from case.city_positions import get_cities
 from util.Chromosome import Chromosome
+from util.natural_selection import pick_parents
 import random
 
 #chromosomes (solutions) are constructed in a way that the traversal is going through cities in order that is in the chromosome.
@@ -31,41 +32,15 @@ for chromosome in population:
 #sorting chromosomes in population according to their fitness value
 population.sort(key=lambda chromosome: chromosome.fitness_value, reverse=True)
 
-#roulette selection
-total_fitness = 0
-for chromosome in population:
-    total_fitness += chromosome.fitness_value
+#picking parents
+parent1, parent2 = pick_parents(population)
 
-cum_sum = []
-for i in range(0, len(population) - 1):
-    sum = 0
-    for j in range (i, len(population)):
-        sum += population[j].fitness_value / total_fitness
-    cum_sum.append(round(sum, 4))
-cum_sum.append(0)
-
-parent1 = 'x'
-parent2 = 'x'
-roulette_ball = random.uniform(0, 1)
-for i in range(1, len(cum_sum)):
-    if roulette_ball >= cum_sum[i]:
-        parent1 = i - 1
-        print(roulette_ball)
-        print(parent1)
-        break
-roulette_ball = random.uniform(0, 1)
-for i in range(1, len(cum_sum)):
-    if roulette_ball >= cum_sum[i]:
-        parent2 = i - 1
-        print(roulette_ball)
-        print(parent2)
-        break
 
 print("INITIAL POPULATION")
 for chromosome in population:
     print(chromosome.__str__())
 
-print(cum_sum)
+print(parent1, parent2)
 
 
 
---
+++ util/natural_selection.py
@@ -0,0 +1,33 @@
+import random
+
+def pick_parents(population):
+    total_fitness = 0
+    for chromosome in population:
+        total_fitness += chromosome.fitness_value
+
+    cum_sum = []
+    for i in range(0, len(population) - 1):
+        sum = 0
+        for j in range (i, len(population)):
+            sum += population[j].fitness_value / total_fitness
+        cum_sum.append(round(sum, 4))
+    cum_sum.append(0)
+
+    parent1 = 'x'
+    parent2 = 'x'
+    
+    roulette_ball = random.uniform(0, 1)
+    for i in range(1, len(cum_sum)):
+        if roulette_ball >= cum_sum[i]:
+            parent1 = i - 1
+            parent2 = i - 1
+            break
+
+    while parent1 == parent2:
+        roulette_ball = random.uniform(0, 1)
+        for i in range(1, len(cum_sum)):
+            if roulette_ball >= cum_sum[i]:
+                parent2 = i - 1
+                break
+
+    return parent1, parent2
